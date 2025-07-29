02902e4beb4cbab2953670bf851cd0fc8460aa21
alonsocv18 <alonso.leo-14@hotmail.com>
2025-07-29 03:02:05 -0500

    home responsive

--- client/src/components/dashboard/DashboardContent.tsx
+++ client/src/components/dashboard/DashboardContent.tsx
@@ -19,10 +19,10 @@ export default function DashboardContent({
         return (
           <div className="p-6">
             <h1 className="text-2xl font-semibold text-gray-800 dark:text-white">
-              Movimientos "POR VER SI LO DEJO"  
+              Movimientos Financieros  
             </h1>
             <p className="mt-2 text-gray-600 dark:text-gray-300">
-              Registra y visualiza todos tus movimientos financieros.
+              Aún estoy por ver si lo dejo
             </p>
           </div>
         )
